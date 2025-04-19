1d3f249b68ff974d64464d5e38900714ecccd99d
yanikan <yanikan3210@gmail.com>
2025-04-19 22:29:13 +0700

    user profile (not complete)

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Sidebar.css";
+import { ReactComponent as xIcon } from "../pictures/xIcon.svg";
 
 // Import PNG icons
 import homeIcon from "../pictures/homeIcon.png";
@@ -10,6 +11,11 @@ import userIcon from "../pictures/userIcon.png";
 import switchIcon from "../pictures/switchIcon.png";
 
 const Sidebar = () => {
+  const [showUserOverlay, setShowUserOverlay] = useState(false);
+  const toggleUserOverlay = () => {
+    setShowUserOverlay(!showUserOverlay);
+  };
+
   return (
     <div className="sidebar-container">
       <div className="sidebar">
@@ -34,7 +40,7 @@ const Sidebar = () => {
           </Link>
           <li className="account-title">Account Page</li>
           <Link to="/user">
-            <li className="user-box">
+            <li className="user-box" onClick={toggleUserOverlay}>
               <img src={userIcon} alt="User" className="w-8 h-8 rounded-full" />
               <span>User xxxx</span>
             </li>
@@ -51,6 +57,27 @@ const Sidebar = () => {
           </Link>
         </ul>
       </div>
+
+      {/* User Overlay */}
+      {showUserOverlay && (
+        <div className="overlay-container">
+          <div className="overlay-content">
+            <div className="close-container" onClick={toggleUserOverlay}>
+              <button className="close-button">
+                <xIcon className="close-icon" />
+              </button>
+            </div>
+            <div className="user-pic">
+              <p>user pic</p>
+            </div>
+            <div className="user-info">
+              <h2>User Name</h2>
+              <p>-- eth</p>
+            </div>
+            <button className="disconnect-button">Disconnect</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
