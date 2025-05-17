dc064f0babba376fc61d3a7cdfa7bc0f2180fd9f
Nawasawan Yenrompho <nawasawan.y@gmail.com>
2025-05-17 13:15:13 +0700

    fix: add sepolia network

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,20 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('@nomicfoundation/hardhat-ethers');
+require("@nomicfoundation/hardhat-ethers");
 require("hardhat-slither");
 require("solidity-coverage");
 
-
-
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.28", 
-
+  solidity: "0.8.28",
   networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+    },
     development: {
-      url: "http://127.0.0.1:7545", 
-      chainId: 1337,           
+      url: "http://127.0.0.1:7545",
+      chainId: 1337,
     },
   },
-  
 };
-
